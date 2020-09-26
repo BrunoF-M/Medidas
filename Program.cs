ae6973d0534e541c0900383e4c178cd90b1fe8ec
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-26 01:55:44 -0300

    Resolução do Execício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double valormetros;
+            
+            Console.Write("digite um valor em Metro(s): ");
+            valormetros = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkYellow;
+            Console.WriteLine("= = = Equivalêcia = = =");
+            Console.ResetColor();
+
+            Console.WriteLine($"{valormetros * 100} cm");
+            Console.WriteLine($"{valormetros} m");
+            Console.WriteLine($"{valormetros / 1000} Km");
+
+            Console.WriteLine();
+            Console.WriteLine("Aperte qualquer tecla para Finalizar...");
+            Console.ReadKey();
+Console.Clear();
+        }
+    }
+}
